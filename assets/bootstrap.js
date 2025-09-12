7d5c36decfee7f531bd630d0c974ab289106e27b
natolotrakeziatsarazaza-cell <natolotrakeziatsarazaza@gmail.com>
2025-09-12 18:53:33 +0200

    Premier envoi

---
+++ assets/bootstrap.js
@@ -0,0 +1,10 @@
+import { startStimulusApp } from '@symfony/stimulus-bridge';
+
+export const app = startStimulusApp(require.context(
+    './controllers',
+    true,
+    /\.(j|t)sx?$/
+));
+// register any custom, 3rd party controllers here
+// app.register('some_controller_name', SomeImportedController);
+// assets/bootstrap.js
